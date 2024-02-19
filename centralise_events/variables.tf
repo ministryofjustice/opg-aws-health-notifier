d5ae763d961413e5b6cccb6cf7671dd8c9015469
Aidan Johnson <37669925+AidanJ97@users.noreply.github.com>
2024-02-19 12:26:21 +0000

    SP-1899 - add tflint to pre commit and workflow #minor (#16)
    * SP-1899 - add tflint to pre commit and workflow #minor
    
    * SP-1899 - tflint fixes #minor

--- centralise_events/variables.tf
+++ centralise_events/variables.tf
@@ -1,10 +1,22 @@
 variable "account_name" {
   description = "AWS account name"
+  type        = string
 }
 
 variable "cross_region_event_bus_role" {
+  description = "Cross region event bus IAM role"
+  type = object({
+    arn  = string
+    name = string
+  })
 }
 
-variable "primary_region_event_bus" {}
+variable "primary_region_event_bus" {
+  description = "Primary region event bus"
+  type = object({
+    arn  = string
+    name = string
+  })
+}
 
 data "aws_region" "current" {}
